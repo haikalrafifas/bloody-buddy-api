fd3e6ee676b6288b03700eed5fbe60ef9d14ed5a
Haikal Rafif <99420525+haikalrafifas@users.noreply.github.com>
2024-06-06 03:30:45 +0700

    ADD - Donor status button information for admins to use

--- app/Http/Resources/DonorStatusResource.php
+++ app/Http/Resources/DonorStatusResource.php
@@ -4,14 +4,24 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class DonorStatusResource extends JsonResource
 {
     public function toArray(Request $request)
     {
-        return [
+        $isAdmin = Auth::user()->is_admin;
+
+        $data = [
             'name' => $this->name,
             'description' => $this->description,
+            'actions' => '',
         ];
+
+        if ( $this->name === 'Waiting List' ) $data['actions'] = $isAdmin ? 'approve,reject' : 'cancel';
+        if ( $this->name === 'Approved' ) $data['actions'] = $isAdmin ? 'start' : 'cancel';
+        if ( $this->name === 'Ongoing' ) $data['actions'] = $isAdmin ? 'done' : '';
+
+        return $data;
     }
 }
